aa2300b04790c9eb452738f2733e4966fddbb179
KortikovaRia <info@D470D@mail.ru>
2021-10-28 17:59:42 +0300

    homework001 done

--- src/main/java/org/example/homework_001.java
+++ src/main/java/org/example/homework_001.java
@@ -2,13 +2,45 @@ package org.example;
 
 public class homework_001 {
     public static void main(String[] args) {
-printThreeWords();
+        printThreeWords();
+        checkSumSign(-3, 14);
+        compareNumbers(10, -9);
+        printColor(123);
+        drawSquare();
     }
 
-    public static void printThreeWords() {
+    static void printThreeWords() {
         System.out.println("Orange");
         System.out.println("Banana");
         System.out.println("Apple");
+
+    }
+
+    static void checkSumSign(int a, int b) {
+        int sum = a + b;
+        if (sum >= 0) {
+            System.out.println("сумма положительна");
+        } else System.out.println("сумма отрицательна");
+    }
+
+    static void compareNumbers(int a, int b) {
+        if (a >= b) {
+            System.out.println("a >= b");
+        } else System.out.println("a < b");
+    }
+
+    static void printColor(int value) {
+        if (value <= 0) {
+            System.out.println("красный");
+        } else if (value > 0 && value <= 100) {
+            System.out.println("желтый");
+        } else System.out.println("зеленый");
+    }
+
+    public static void drawSquare() {
+        System.out.println("  0");
+        System.out.println("--|--");
+        System.out.println(" /|");
 
     }
 }
